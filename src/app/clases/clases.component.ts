a3c25d41ce0a2050c0ec639e6d135b81e7766857
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-21 20:24:46 -0400

    class binding

---
+++ src/app/clases/clases.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html',
+  styleUrls: ['./clases.component.css']
+})
+export class ClasesComponent {
+  src = "https://images.pexels.com/photos/15185102/pexels-photo-15185102.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+  img1 = "https://images.pexels.com/photos/16407235/pexels-photo-16407235.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+  img2 = "https://images.pexels.com/photos/5546810/pexels-photo-5546810.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+}
