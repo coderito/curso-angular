ada4f7b89bc2d15ca5f4326877c088be62208c6e
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-21 20:23:01 -0400

    importando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ContadorComponent } from './contador/contador.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
+import { BotonesComponent } from './botones/botones.component';
+import { ClasesComponent } from './clases/clases.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { BuclesComponent } from './bucles/bucles.component';
+import { SwitchComponent } from './switch/switch.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    EjemploComponent,
+    BotonesComponent,
+    ClasesComponent,
+    FormularioComponent,
+    BuclesComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule
